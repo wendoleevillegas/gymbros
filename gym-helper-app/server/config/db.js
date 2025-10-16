bffa6844c9499049213e7e72a1a6cd00d5b98d3d
Bibhav Shrestha <bxs4106@mavs.uta.edu>
2025-10-16 12:42:04 -0500

    Database connection setup with mongoose

---
+++ gym-helper-app/server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Database connected successfully | " + connection.connection.host);
+    } catch (error) {
+        console.log("Error connecting to database" + error);
+        process.exit(1);
+    }
+}
+
+export default connectToDB;
