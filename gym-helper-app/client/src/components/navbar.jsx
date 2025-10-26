1136189b571060345801e05d94425e92c2617c5c
Maryam <maryam@bytesquad.dev>
2025-10-27 01:43:16 +0500

    Navbar UI

--- gym-helper-app/client/src/components/navbar.jsx
+++ gym-helper-app/client/src/components/navbar.jsx
@@ -6,27 +6,27 @@ import { GrHistory } from "react-icons/gr";
 
 export default function Navbar() {
   return (
-    <nav className="bg-white dark:bg-black fixed bottom-0 w-full py-4 px-8 z-50">
-      <ul className="flex justify-between w-[95%]">
-        <li>
+    <nav className="bg-white dark:bg-black text-black dark:text-white fixed bottom-0 left-0 w-full py-4 px-8 z-50">
+      <ul className="list-none flex justify-between items-center w-[95%] p-0 m-0">
+        <li className = "Workout">
           <Link to="/workout" className="flex flex-col items-center">
             <GiWeightLiftingUp className="w-8 h-8 mb-1" />
             <span>Workout</span>
           </Link>
         </li>
-        <li>
+        <li className = "Profile">
           <Link to="/profile" className="flex flex-col items-center">
             <IoPersonOutline className="w-8 h-8 mb-1" />
             <span>Profile</span>
           </Link>
         </li>
-        <li>
+        <li className = "Home">
           <Link to="/" className="flex flex-col items-center">
             <HiHome className="w-8 h-8 mb-1" />
             <span>Home</span>
           </Link>
         </li>
-        <li>
+        <li className = "History">
           <Link to="/history" className="flex flex-col items-center">
             <GrHistory className="w-8 h-8 mb-1" />
             <span>History</span>
