0872f58fe098faffb1464cad97ed561503558c3e
Xterionix <xterionyt@gmail.com>
2025-11-19 14:01:26 -0600

    - Setup authcontext to return user object

--- gym-helper-app/client/src/contexts/theme/AuthContext.jsx
+++ gym-helper-app/client/src/contexts/theme/AuthContext.jsx
@@ -1,20 +1,61 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+
+export const unknownUser = {
+    name: "???",
+    username: "@unknown",
+    email: "???@unknown.com",
+    profilePicture: "/assets/croc-dark.png",
+}
 
 // Creating context
 const AuthContext = createContext(null);
 
 // Creating provider component
 export const AuthProvider = ({ children }) => {
-    // checking local storage for the initial state
-    const [isAuth, setIsAuth] = useState(() => {
-        return localStorage.getItem("isAuth") === "true";
-    });
 
-    // later on, isAuth will be replaced with the 'User' object, and we'll add 
-    // login/logout functions that will call the API
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchUserData() {
+            console.log('Fetching user data')
+            try {
+                const res = await fetch("http://localhost:5000/api/auth/me", { credentials: "include" });
+                if (!res.ok) {
+                    setUser(null);
+                    return;
+                }
+                const json = await res.json();
+
+                if (json.status != 200) { setUser(null); return };
+                setUser(json.data);
+            } catch (err) {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchUserData();
+    }, [])
+
+    function login() {
+        window.location.href = "http://localhost:5000/api/auth/google";
+    }
+
+    async function logout() {
+        try {
+            await fetch('http://localhost:5000/api/auth/logout', { method: 'POST', credentials: 'include' });
+        } catch (e) {
+            console.log('Logout failed', e);
+        } finally {
+            setUser(null);
+            window.location.href = '/login';
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{ isAuth, setIsAuth }}>
-        {children}
+        <AuthContext.Provider value={{ user, loading, login, logout }}>
+            {children}
         </AuthContext.Provider>
     );
 };
