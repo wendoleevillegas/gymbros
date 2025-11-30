46e7428781648c28933478f4f44cd32d1ca5987d
Wendolee <167564740+wendoleevillegas@users.noreply.github.com>
2025-11-30 23:28:43 +0000

    trying to fix mongodb err

--- gym-helper-app/server/config/db.js
+++ gym-helper-app/server/config/db.js
@@ -5,7 +5,7 @@ const connectToDB = async () => {
         const connection = await mongoose.connect(process.env.MONGODB_URL);
         console.log("Database connected successfully | " + connection.connection.host);
     } catch (error) {
-        console.log("Error connecting to database" + error);
+        console.log("Error connecting to database " + error);
         process.exit(1);
     }
 }
